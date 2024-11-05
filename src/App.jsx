21e4a8266947e0064e8eac71d967a949bb02306e
Bakha022 <abduqahhorovbahodir536@gmail.com>
2024-11-05 16:08:29 +0500

    notFound page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import AdminLayout from './components/layout/AdminLayout'
 import PublicLayout from './components/layout/PublicLayout'
+import DashboardPage from './pages/protected/admin/DashboardPage'
 import BlogsPage from './pages/public/BlogsPage'
 import CategoryPage from './pages/public/CategoryPage'
 import HomePage from './pages/public/HomePage'
 import LoginPage from './pages/public/LoginPage'
 import NotFoundPage from './pages/public/NotFoundPage'
 import RegisterPage from './pages/public/RegisterPage'
+
 const App = () => {
 	return (
 		<>
@@ -20,6 +23,11 @@ const App = () => {
 						<Route path='/login' element={<LoginPage />} />
 						<Route path='/register' element={<RegisterPage />} />
 					</Route>
+
+					<Route element={<AdminLayout />}>
+						<Route path='dashboard' element={<DashboardPage />} />
+					</Route>
+
 					<Route path='*' element={<NotFoundPage />} />
 				</Routes>
 			</BrowserRouter>
--- src/components/layout/AdminLayout.jsx
+++ src/components/layout/AdminLayout.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const AdminLayout = () => {
-	return <div>AdminLayout</div>
+	return (
+		<div>
+			<Outlet />
+		</div>
+	)
 }
 
 export default AdminLayout
