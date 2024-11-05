17c17c26e52b56189be970059a77903fcb845623
Bakha022 <abduqahhorovbahodir536@gmail.com>
2024-11-05 22:50:33 +0500

    done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,58 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
 	return (
-		<div>Navbar</div>
+		<nav className='py-6 bg-[#232536]'>
+			<div className='container'>
+				<div className='flex items-center justify-between'>
+					<div className='logo'>
+						<Link to='/'>
+							<img src='/Logo.svg' alt='logo' />
+						</Link>
+					</div>
+					<ul className='flex gap-[24px] items-center'>
+						<li>
+							<Link
+								className='font-normal text-sen text-white text-base leading-7 hover:text-gray-400'
+								to='/'
+							>
+								Home
+							</Link>
+						</li>
+						<li>
+							<Link
+								className='font-normal text-sen text-white text-base leading-7 hover:text-gray-400'
+								to='/blog'
+							>
+								Blog
+							</Link>
+						</li>
+						<li>
+							<Link
+								className='font-normal text-sen text-white text-base leading-7 hover:text-gray-400'
+								to='/about'
+							>
+								About Us
+							</Link>
+						</li>
+						<li>
+							<Link
+								className='font-normal text-sen text-white text-base leading-7 hover:text-gray-400'
+								to='/register'
+							>
+								Register
+							</Link>
+						</li>
+						<li className='bg-white py-4 px-12'>
+							<Link className='text-inter text-[#232536] font-bold' to='/login'>
+								Login
+							</Link>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
 	)
 }
 
